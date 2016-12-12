14debdd27c31651a67b62b38737ed255a3e9eb02
Dipak Moliya <dipakmoliya@outlook.com>
2016-12-12 11:01:31 +0530

    Comments added

--- Ch02_Basics/Program.cs
+++ Ch02_Basics/Program.cs
@@ -9,8 +9,8 @@ namespace Ch02_Basics
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello C#");
-        }
+        {   // the start of a block
+            Console.WriteLine("Hello C#");  // a statement
+        }   // the end of a block
     }
 }
